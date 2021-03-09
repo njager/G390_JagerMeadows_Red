f6634caef53f8355a14419b9cc77ed0c327d8837
aemeadow <aemeadow@iu.edu>
2021-03-08 22:14:57 -0500

    objects

---
+++ G390_JagerMeadows_Red/Assets/objects/dooropen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class dooropen : MonoBehaviour
+{
+    public Component[] Lights;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "rollydude")
+        {
+            //add the code you want to execute on collision
+
+            Animator anim = GetComponentInChildren<Animator>();
+            anim.SetTrigger("OpenClose");
+
+
+            //to access the Ball gameObject use : col.gameObject
+            foreach (Light l in this.GetComponentsInChildren<Light>())
+                l.intensity = 7;
+
+        }
+        else
+        {
+            
+            foreach (Light l in this.GetComponentsInChildren<Light>())
+                l.intensity = 0;
+        }
+
+        }
+    }
